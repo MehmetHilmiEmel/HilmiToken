5650c5316b8d2e7c41bc6f2fdc180da179023f39
Mehmet Hilmi Emel <86683701+MehmetHilmiEmel@users.noreply.github.com>
2022-02-16 17:03:44 +0300

    Update HilmiToken.sol

--- HilmiToken.sol
+++ HilmiToken.sol
@@ -1,92 +1,387 @@
-pragma solidity >=0.4.22 <0.6.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.5;
+	
 
-interface tokenRecipient { 
-    function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; 
-}
+	/**
+	 * @dev Wrappers over Solidity's arithmetic operations with added overflow
+	 * checks.
+	 *
+	 * Arithmetic operations in Solidity wrap on overflow. This can easily result
+	 * in bugs, because programmers usually assume that an overflow raises an
+	 * error, which is the standard behavior in high level programming languages.
+	 * `SafeMath` restores this intuition by reverting the transaction when an
+	 * operation overflows.
+	 *
+	 * Using this library instead of the unchecked operations eliminates an entire
+	 * class of bugs, so it's recommended to use it always.
+	 */
+	library SafeMath {
+	    /**
+	     * @dev Returns the addition of two unsigned integers, reverting on
+	     * overflow.
+	     *
+	     * Counterpart to Solidity's `+` operator.
+	     *
+	     * Requirements:
+	     * - Addition cannot overflow.
+	     */
+	    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+	        uint256 c = a + b;
+	        require(c >= a, "SafeMath: addition overflow");
+	
 
-contract TokenERC20 {
-    string public name = "Hilmi Token";
-    string public symbol= "HLM";
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
+	        return c;
+	    }
+	
 
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+	    /**
+	     * @dev Returns the subtraction of two unsigned integers, reverting on
+	     * overflow (when the result is negative).
+	     *
+	     * Counterpart to Solidity's `-` operator.
+	     *
+	     * Requirements:
+	     * - Subtraction cannot overflow.
+	     */
+	    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+	        return sub(a, b, "SafeMath: subtraction overflow");
+	    }
+	
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+	    /**
+	     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+	     * overflow (when the result is negative).
+	     *
+	     * Counterpart to Solidity's `-` operator.
+	     *
+	     * Requirements:
+	     * - Subtraction cannot overflow.
+	     *
+	     * _Available since v2.4.0._
+	     */
+	    function sub(
+	        uint256 a,
+	        uint256 b,
+	        string memory errorMessage
+	    ) internal pure returns (uint256) {
+	        require(b <= a, errorMessage);
+	        uint256 c = a - b;
+	
 
-    event Burn(address indexed from, uint256 value);
+	        return c;
+	    }
+	
 
-    constructor(
-        uint256 initialSupply,
+	    /**
+	     * @dev Returns the multiplication of two unsigned integers, reverting on
+	     * overflow.
+	     *
+	     * Counterpart to Solidity's `*` operator.
+	     *
+	     * Requirements:
+	     * - Multiplication cannot overflow.
+	     */
+	    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+	        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+	        // benefit is lost if 'b' is also tested.
+	        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+	        if (a == 0) {
+	            return 0;
+	        }
+	
 
-    ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  
-        balanceOf[msg.sender] = totalSupply;                
-    }
+	        uint256 c = a * b;
+	        require(c / a == b, "SafeMath: multiplication overflow");
+	
 
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != address(0x0));
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
+	        return c;
+	    }
+	
 
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
+	    /**
+	     * @dev Returns the integer division of two unsigned integers. Reverts on
+	     * division by zero. The result is rounded towards zero.
+	     *
+	     * Counterpart to Solidity's `/` operator. Note: this function uses a
+	     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+	     * uses an invalid opcode to revert (consuming all remaining gas).
+	     *
+	     * Requirements:
+	     * - The divisor cannot be zero.
+	     */
+	    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+	        return div(a, b, "SafeMath: division by zero");
+	    }
+	
+
+	    /**
+	     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+	     * division by zero. The result is rounded towards zero.
+	     *
+	     * Counterpart to Solidity's `/` operator. Note: this function uses a
+	     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+	     * uses an invalid opcode to revert (consuming all remaining gas).
+	     *
+	     * Requirements:
+	     * - The divisor cannot be zero.
+	     *
+	     * _Available since v2.4.0._
+	     */
+	    function div(
+	        uint256 a,
+	        uint256 b,
+	        string memory errorMessage
+	    ) internal pure returns (uint256) {
+	        // Solidity only automatically asserts when dividing by 0
+	        require(b > 0, errorMessage);
+	        uint256 c = a / b;
+	        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+	
+
+	        return c;
+	    }
+	
+
+	    /**
+	     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+	     * Reverts when dividing by zero.
+	     *
+	     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+	     * opcode (which leaves remaining gas untouched) while Solidity uses an
+	     * invalid opcode to revert (consuming all remaining gas).
+	     *
+	     * Requirements:
+	     * - The divisor cannot be zero.
+	     */
+	    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+	        return mod(a, b, "SafeMath: modulo by zero");
+	    }
+	
+
+	    /**
+	     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+	     * Reverts with custom message when dividing by zero.
+	     *
+	     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+	     * opcode (which leaves remaining gas untouched) while Solidity uses an
+	     * invalid opcode to revert (consuming all remaining gas).
+	     *
+	     * Requirements:
+	     * - The divisor cannot be zero.
+	     *
+	     * _Available since v2.4.0._
+	     */
+	    function mod(
+	        uint256 a,
+	        uint256 b,
+	        string memory errorMessage
+	    ) internal pure returns (uint256) {
+	        require(b != 0, errorMessage);
+	        return a % b;
+	    }
+	}
+	
+
+	abstract contract Owned {
+	    address public owner;
+	
+
+	    constructor() {
+	        owner = msg.sender;
+	    }
+	
+
+	    modifier onlyOwner {
+	        require(msg.sender == owner);
+	        _;
+	    }
+	    
+	}
+	
+
+	interface ERC20Interface {
+	    function totalSupply() external view returns (uint256);
+	
+
+	    function balanceOf(address tokenOwner)
+	        external
+	        view
+	        returns (uint256 balance);
+	
+
+	    function allowance(address tokenOwner, address spender)
+	        external
+	        view
+	        returns (uint256 remaining);
+	
+
+	    function transfer(address to, uint256 tokens)
+	        external
+	        returns (bool success);
+	
+
+	    function approve(address spender, uint256 tokens)
+	        external
+	        returns (bool success);
+	
+
+	    function increaseAllowance(address spender, uint256 tokens)
+	        external
+	        returns (bool success);
+	
+
+	    function decreaseAllowance(address spender, uint256 tokens)
+	        external
+	        returns (bool success);
+	
+
+	    function transferFrom(
+	        address from,
+	        address to,
+	        uint256 tokens
+	    ) external returns (bool success);
+	
+
+	    event Transfer(address indexed from, address indexed to, uint256 tokens);
+	    event Approval(
+	        address indexed tokenOwner,
+	        address indexed spender,
+	        uint256 tokens
+	    );
+	}
+	
+
+	interface ApproveAndCallFallBack {
+	    function receiveApproval(
+	        address from,
+	        uint256 tokens,
+	        address token,
+	        bytes memory data
+	    ) external;
+	}
+	
+
+	contract Metaplace is ERC20Interface, Owned {
+	    using SafeMath for uint256;
+	
+
+	    string public symbol = "BT";
+	    string public name = "Benim Tokenim";
+	    uint8 public decimals = 18;
+	    uint256 DEC = 10**uint256(decimals);
+	    uint256 public _totalSupply = 100000000000 * DEC;
+	
+
+	    mapping(address => uint256) balances;
+	    mapping(address => mapping(address => uint256)) allowed;
+	
+
+	    constructor() {
+	        balances[owner] = _totalSupply;
+	        emit Transfer(address(0x0), owner, _totalSupply);
+	    }
+	
+
+	    function totalSupply() public view override returns (uint256) {
+	        return _totalSupply;
+	    }
+	
+
+	    function balanceOf(address tokenOwner)
+	        public
+	        view
+	        override
+	        returns (uint256 balance)
+	    {
+	        return balances[tokenOwner];
+	    }
+	
+
+	    function transfer(address to, uint256 tokens)
+	        public
+	        override
+	        returns (bool success)
+	    {
+	        balances[msg.sender] = balances[msg.sender].sub(tokens);
+	        balances[to] = balances[to].add(tokens);
+	        emit Transfer(msg.sender, to, tokens);
+	        return true;
+	    }
+	
+
+	    function approve(address spender, uint256 tokens)
+	        public
+	        override
+	        returns (bool success)
+	    {
+	        allowed[msg.sender][spender] = tokens;
+	        emit Approval(msg.sender, spender, tokens);
+	        return true;
+	    }
+	
+
+	    function increaseAllowance(address spender, uint256 amount)
+	        public
+	        override
+	        returns (bool success)
+	    {
+	        return approve(spender, allowed[msg.sender][spender].add(amount));
+	    }
+	
+
+	    function decreaseAllowance(address spender, uint256 amount)
+	        public
+	        override
+	        returns (bool success)
+	    {
+	        return approve(spender, allowed[msg.sender][spender].sub(amount));
+	    }
+	
+
+	    function transferFrom(
+	        address from,
+	        address to,
+	        uint256 tokens
+	    ) public override returns (bool success) {
+	        balances[from] = balances[from].sub(tokens);
+	        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+	        balances[to] = balances[to].add(tokens);
+	        emit Transfer(from, to, tokens);
+	        return true;
+	    }
+	
+
+	    function allowance(address tokenOwner, address spender)
+	        public
+	        view
+	        override
+	        returns (uint256 remaining)
+	    {
+	        return allowed[tokenOwner][spender];
+	    }
+	
+
+	    function approveAndCall(
+	        address spender,
+	        uint256 tokens,
+	        bytes memory data
+	    ) public returns (bool success) {
+	        approve(spender, tokens);
+	        ApproveAndCallFallBack(spender).receiveApproval(
+	            msg.sender,
+	            tokens,
+	            address(this),
+	            data
+	        );
+	        return true;
+	    }
+	
+
+	    event LogInt(string, uint256);
+	}
 
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
 
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
 
 
-    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, address(this), _extraData);
-            return true;
-        }
-    }
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   
-        balanceOf[msg.sender] -= _value;            
-        totalSupply -= _value;                      
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                
-        require(_value <= allowance[_from][msg.sender]);    
-        balanceOf[_from] -= _value;                         
-        allowance[_from][msg.sender] -= _value;             
-        totalSupply -= _value;                              
-        emit Burn(_from, _value);
-        return true;
-    }
-}
